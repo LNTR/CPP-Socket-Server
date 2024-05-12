fc84c7963b452b031695ca393be7be2bc2df6932
LNTR <101091694+LNTR@users.noreply.github.com>
2024-05-12 12:42:05 +0530

    Boosts With Socket Server

--- server.cpp
+++ server.cpp
@@ -1,10 +1,41 @@
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <stdio.h>
+#include <iostream>
+#include <boost/asio.hpp>
 
-#pragma comment(lib, "Ws2_32.lib")
+using namespace boost::asio;
+using ip::tcp;
+using std::cout;
+using std::endl;
+using std::string;
+
+string read_(tcp::socket &socket)
+{
+    boost::asio::streambuf buf;
+    boost::asio::read_until(socket, buf, "\n");
+
+    string data = boost::asio::buffer_cast<const char *>(buf.data());
+    return data;
+}
+
+void send_(tcp::socket &socket, const string &message)
+{
+    const string msg = message + "\n";
+    boost::asio::write(socket, boost::asio::buffer(message));
+}
 
 int main()
 {
+    boost::asio::io_service io_service;
+
+    tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 1234));
+
+    tcp::socket socket_(io_service);
+    acceptor_.accept(socket_);
+
+    string message = read_(socket_);
+    cout << message << endl;
+
+    send_(socket_, "Hello From Server!");
+    cout << "Servent sent Hello message to Client!" << endl;
+
     return 0;
 }
